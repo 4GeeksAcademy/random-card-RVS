e659459b3ca23965729e1ec8ccfd19904afa9c66
Roberto J. Vargas D <rvargas@rv-solutions.net>
2023-08-18 17:44:01 +0000

    Proyecto Terminado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,55 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
+import "../src/assets/img/logorvs.png";
 import "./assets/img/4geeks.ico";
 
+function getRandomNumber() {
+  const numbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+function getRandomImage() {
+  const images = ["♦", "♥️", "♠️", "♣️"];
+  return images[Math.floor(Math.random() * images.length)];
+}
+
+function tarjetas() {
+  const numero = document.querySelector(".numero");
+  const imagen1 = document.querySelector(".imagenes1");
+  const imagen2 = document.querySelector(".imagenes2");
+
+  const numeros = getRandomNumber();
+  const image = getRandomImage();
+
+  numero.innerHTML = numeros;
+  imagen1.innerHTML = image;
+  imagen2.innerHTML = image;
+
+  const color = ["red", "black"][Math.floor(Math.random() * 2)];
+  imagen1.style.color = color;
+  imagen2.style.color = color;
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  for (let i = 0; i < "Random Card RVS".length; i++) {
+    setTimeout(() => {
+      document.querySelector(".titulo").innerHTML += "Random Card RVS"[i];
+    }, i * 100);
+    tarjetas();
+  }
 };
